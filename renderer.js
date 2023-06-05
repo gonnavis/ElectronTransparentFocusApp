8311bdf57c814c94766018fde4179fee00b0c5c3
gonnavis <gonnavis@163.com>
2023-06-05 17:39:53 +0800

    dev

---
+++ renderer.js
@@ -0,0 +1,19 @@
+
+const button = document.querySelector('button');
+const div = document.querySelector('div');
+button.addEventListener('click', () => {
+  // set document body background color to random color
+  // document.body.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 75%)`;
+  // document.body.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  
+  // create a new div and add to body with text content
+  const newDiv = document.createElement('div');
+  newDiv.textContent = 'Hello World';
+  document.body.appendChild(newDiv);
+})
+button.addEventListener('mouseenter', () => {
+  div.textContent = 'Mouse entered';
+})
+button.addEventListener('mouseleave', () => {
+  div.textContent = 'Mouse left';
+})
